e39b47b0290ce9ac8a5cb34bfb29e315cbddaf7a
Lauren Gallagher <109532911+laurengall@users.noreply.github.com>
2023-05-12 11:52:53 -0400

    Add files via upload

---
+++ Athlete(1).java
@@ -0,0 +1,38 @@
+import java.util.ArrayList; 
+public class Athlete extends Person {
+  private int age; 
+  private int grade;
+  private ArrayList<String> events = new ArrayList<String>(); 
+
+  public Athlete(String name){
+    super(name); 
+    this.age = age;
+    this.grade = grade; 
+    this.events = events; 
+  }
+  public Athlete(String name, int a, int g, ArrayList<String> e){
+    super(name); 
+    age = a; 
+    grade = g; 
+    events = e; 
+  }
+  
+  public int getAge(){
+    return age; 
+  }
+  public void setAge(int age){
+    this.age = age; 
+  }
+  public int getGrade(){
+    return grade; 
+  }
+  public void setGrade(int grade){
+    this.grade = grade; 
+  }
+  public ArrayList<String> getEvents(){
+    return events; 
+  }
+  public void setEvents(ArrayList<String> events){
+    this.events = events; 
+  }
+}
---
+++ Person.java
@@ -0,0 +1,15 @@
+public class Person {
+  private String name; 
+  public Person(){
+    this.name = name; 
+  }
+  public Person(String n){
+    name = n; 
+  }
+  public String getName(){
+    return name; 
+  }
+  public void setName(String name){
+    this.name = name; 
+  }
+}
