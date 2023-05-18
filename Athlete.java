b3af9265b05123c8b4e9355e51bf4dbb664cc1b8
Lauren Gallagher <lauren.21.gallagher@gmail.com>
2023-05-18 08:54:35 -0400

    commit

--- Athlete.java
+++ Athlete.java
@@ -1,38 +1,45 @@
-import java.util.ArrayList; 
-public class Athlete extends Person {
-  private int age; 
-  private int grade;
-  private ArrayList<String> events = new ArrayList<String>(); 
+import java.util.ArrayList;
 
-  public Athlete(String name){
-    super(name); 
+public class Athlete extends Person {
+  private int age;
+  private int grade;
+  private ArrayList<String> events = new ArrayList<String>();
+
+  public Athlete(String name) {
+    super(name);
     this.age = age;
-    this.grade = grade; 
-    this.events = events; 
+    this.grade = grade;
+    this.events = events;
   }
-  public Athlete(String name, int a, int g, ArrayList<String> e){
-    super(name); 
-    age = a; 
-    grade = g; 
-    events = e; 
+
+  public Athlete(String name, int a, int g, ArrayList<String> e) {
+    super(name);
+    age = a;
+    grade = g;
+    events = e;
   }
-  
-  public int getAge(){
-    return age; 
+
+  public int getAge() {
+    return age;
   }
-  public void setAge(int age){
-    this.age = age; 
+
+  public void setAge(int age) {
+    this.age = age;
   }
-  public int getGrade(){
-    return grade; 
+
+  public int getGrade() {
+    return grade;
   }
-  public void setGrade(int grade){
-    this.grade = grade; 
+
+  public void setGrade(int grade) {
+    this.grade = grade;
   }
-  public ArrayList<String> getEvents(){
-    return events; 
+
+  public ArrayList<String> getEvents() {
+    return events;
   }
-  public void setEvents(ArrayList<String> events){
-    this.events = events; 
+
+  public void setEvents(ArrayList<String> events) {
+    this.events = events;
   }
 }
